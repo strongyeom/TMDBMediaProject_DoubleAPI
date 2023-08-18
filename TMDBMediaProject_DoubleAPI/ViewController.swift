7cd269f4cef701d776abd680c079ddda4b94054b
yeomsungpil <present7256@gmail.com>
2023-08-18 15:22:59 +0900

    Initial Commit

---
+++ TMDBMediaProject_DoubleAPI/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TMDBMediaProject_DoubleAPI
+//
+//  Created by 염성필 on 2023/08/18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
